6ec0a29402485a52138a5a2be874eb92ee8ccf02
James Humphry <git@binecho.co.uk>
2016-02-19 23:48:44 +0000

    Document expected semantics in Counters_Spec

--- src/counters_spec.ads
+++ src/counters_spec.ads
@@ -24,22 +24,36 @@ generic
    type Counter_Ptr is access Counter;
 
    with function Make_New_Counter(Element : T_Ptr) return Counter_Ptr is <>;
+   -- Make a new counter that points to the Element
 
    with procedure Deallocate_If_Unused (C : in out Counter_Ptr) is <>;
+   -- Deallocate the Counter object if the Use_Count and Weak_Ptr_Count are
+   -- both zero
 
    with function Element(C : in Counter) return T_Ptr is <>;
+   -- Return the Element pointed to by the counter
 
    with function Use_Count (C : in Counter) return Natural is <>;
+   -- Return the number of Smart_Ptr / Smart_Ref using the counter
 
    with procedure Increment_Use_Count (C : in out Counter) is <>;
+   -- Increment the Use_Count if the current use count is greater than zero,
+   -- otherwise do nothing. These sematics help the Weak_Ptr.Lock code ensure
+   -- that the target object does not get deallocated between the Weak_Ptr
+   -- routine checking that it still exists (Use_Count > 0) and creating a
+   -- new Smart_Ptr to it.
 
    with procedure Decrement_Use_Count (C : in out Counter) is <>;
+   -- Decrement the Use_Count of the counter
 
    with function Weak_Ptr_Count (C : in Counter) return Natural is <>;
+   -- Return the number of Weak_Ptr using the counter
 
    with procedure Increment_Weak_Ptr_Count (C : in out Counter) is <>;
+   -- Increment the Weak_Ptr_Count of the counter, unconditionally.
 
    with procedure Decrement_Weak_Ptr_Count (C : in out Counter) is <>;
+   -- Decrement the Weak_Ptr_Count of the counter
 
 package Counters_Spec is
 
