81e848aac20e763694af10a20e02f8e451a84165
James Humphry <git@binecho.co.uk>
2016-02-20 22:34:51 +0000

    Add unit tests for protected counters

--- tests/auto_counters_suite.adb
+++ tests/auto_counters_suite.adb
@@ -7,6 +7,7 @@ with Smart_Ptrs_Tests;
 with Auto_Counters_Suite.Unique_Ptrs_Tests;
 
 with Basic_Counters;
+with Protected_Counters;
 
 package body Auto_Counters_Suite is
    use AUnit.Test_Suites;
@@ -19,7 +20,15 @@ package body Auto_Counters_Suite is
      new Smart_Ptrs_Tests(Counters => String_Basic_Counters.Basic_Counters_Spec,
                           Counter_Type_Name => "basic counters");
 
-   Test_Smart_Ptrs : aliased Basic_Smart_Ptrs_Tests.Smart_Ptrs_Test;
+   Test_Basic_Smart_Ptrs : aliased Basic_Smart_Ptrs_Tests.Smart_Ptrs_Test;
+
+   package String_Protected_Counters is new Protected_Counters(T => String,
+                                                               T_Ptr => String_Ptr);
+   package Protected_Smart_Ptrs_Tests is
+     new Smart_Ptrs_Tests(Counters => String_Protected_Counters.Protected_Counters_Spec,
+                          Counter_Type_Name => "protected counters");
+
+   Test_Protected_Smart_Ptrs : aliased Protected_Smart_Ptrs_Tests.Smart_Ptrs_Test;
 
    Test_Unique_Ptrs : aliased Unique_Ptrs_Tests.Unique_Ptrs_Test;
 
@@ -27,7 +36,8 @@ package body Auto_Counters_Suite is
 
    function Suite return AUnit.Test_Suites.Access_Test_Suite is
    begin
-      Add_Test (Result'Access, Test_Smart_Ptrs'Access);
+      Add_Test (Result'Access, Test_Basic_Smart_Ptrs'Access);
+      Add_Test (Result'Access, Test_Protected_Smart_Ptrs'Access);
       Add_Test (Result'Access, Test_Unique_Ptrs'Access);
       return Result'Access;
    end Suite;
